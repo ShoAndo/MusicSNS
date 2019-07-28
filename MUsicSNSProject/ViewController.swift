bfdc8b91ff866ea7d0cbacbe061441df79f2868b
Sho Ando <spdtas0223@gmail.com>
2019-07-28 11:32:02 +0800

    できない

---
+++ MUsicSNSProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MUsicSNSProject
+//
+//  Created by 安藤奨 on 2019/07/28.
+//  Copyright © 2019 安藤奨. All rights reserved.
+//
+
+import UIKit
+import Koloda
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var kolodaView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url: URL = URL(string: "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch?")!
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
